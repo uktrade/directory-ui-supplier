cb7ad54aa3967b8301dcf77087650ab02a754cd6
Rich Tier <rikatee@gmail.com>
2017-03-08 15:43:01 +0000

    Add adnonymous notifications unsubscribe view

---
+++ notifications/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class AnonymousUnsubscribeForm(forms.Form):
+	# not using EmailField because the value is signed
+	email = forms.CharField(widget=forms.HiddenInput())
---
+++ notifications/views.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.views.generic.edit import FormView
+
+from notifications import forms
+
+from api_client import api_client
+
+
+class AnonymousUnsubscribeView(FormView):
+    form_class = forms.AnonymousUnsubscribeForm
+    template_name = 'anonymous_unsubscribe.html'
+    success_template_name = 'anonymous_unsubscribe_success.html'
+    failure_template_name = 'anonymous_unsubscribe_error.html'
+
+    def get_initial(self):
+        return {
+            'email': self.request.GET['email']
+        }
+
+    def dispatch(self, *args, **kwargs):
+        if not settings.FEATURE_UNSUBSCRIBE_VIEW_ENABLED:
+            raise Http404()
+        if not self.request.GET.get('email'):
+            return TemplateResponse(self.request, self.failure_template_name)
+        return super().dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        response = api_client.notifications.anonymous_unsubscribe(
+            email=form.cleaned_data['email']
+        )
+        if response.ok:
+            return TemplateResponse(self.request, self.success_template_name)
+        return TemplateResponse(self.request, self.failure_template_name)
