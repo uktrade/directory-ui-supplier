d315e84e13c0b2e3afe30fe2da54d4d5fc7a9367
Richard Tier <rihard@highertier.com>
2017-07-17 09:55:34 +0100

    Update links on industry pages to cater for search with sector filter feature flag

--- ui/tests/test_context_processors.py
+++ ui/tests/test_context_processors.py
@@ -14,6 +14,7 @@ def test_feature_returns_expected_features(rf, settings):
 
     settings.FEATURE_MORE_INDUSTRIES_BUTTON_ENABLED = True
     settings.FEATURE_COMPANY_SEARCH_VIEW_ENABLED = False
+    settings.FEATURE_SEARCH_FILTER_SECTOR_ENABLED = True
     request = rf.get('/')
     actual = context_processors.feature_flags(request)
 
@@ -21,6 +22,7 @@ def test_feature_returns_expected_features(rf, settings):
         'features': {
             'FEATURE_MORE_INDUSTRIES_BUTTON_ENABLED': True,
             'FEATURE_COMPANY_SEARCH_VIEW_ENABLED': False,
+            'FEATURE_SEARCH_FILTER_SECTOR_ENABLED': True,
         },
     }
 
