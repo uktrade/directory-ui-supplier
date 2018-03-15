9db87b7ebfe3ee528096dd5f73c443129e782256
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2018-03-15 17:08:06 +0000

    redis from url

--- ui/settings.py
+++ ui/settings.py
@@ -358,10 +358,7 @@ THUMBNAIL_FORCE_OVERWRITE = True
 
 # Redis for thumbnails cache
 if os.getenv('REDIS_URL'):
-    redis_url = urlparse(os.environ['REDIS_URL'])
-    THUMBNAIL_REDIS_PORT = redis_url.port
-    THUMBNAIL_REDIS_HOST = redis_url.hostname
-    THUMBNAIL_REDIS_PASSWORD = redis_url.password or ''
+    THUMBNAIL_REDIS_URL = os.environ['REDIS_URL']
 
 # django-storages for thumbnails
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
