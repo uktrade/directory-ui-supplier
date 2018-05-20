e22a1674a82775447279dd71adea83f8cf772265
richtier <rikatee@gmail.com>
2018-05-20 11:21:32 +0100

    Fix cmd-webserver

--- docker/cmd-webserver.sh
+++ docker/cmd-webserver.sh
@@ -2,4 +2,4 @@
 
 python /usr/src/app/manage.py collectstatic --noinput
 python /usr/src/app/manage.py compilemessages
-gunicorn ui.wsgi --bind 0.0.0.0:$PORT --log-file -
+gunicorn conf.wsgi --bind 0.0.0.0:$PORT --log-file -
