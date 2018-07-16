5dd153b444502196ebfd27257d3df3c806275c10
eadpearce <e.pearce@tutanota.com>
2018-07-16 12:59:26 +0100

    Add context processor for html lang and tests

--- core/tests/test_context_processors.py
+++ core/tests/test_context_processors.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import reverse
+from django.utils import translation
 
 from core import context_processors, forms
 
@@ -33,3 +34,20 @@ def test_lead_generation_form_exposes_form_details(rf):
     assert isinstance(
         actual['lead_generation']['form'], forms.LeadGenerationForm
     )
+
+
+def test_html_lang_attribute_processor_default_lang(rf):
+    request = rf.get(reverse('index'))
+
+    actual = context_processors.html_lang_attribute(request)
+
+    assert actual['directory_components_html_lang_attribute'] == 'en-gb'
+
+
+def test_html_lang_attribute_processor_set_lang(rf):
+    translation.activate('fr')
+    request = rf.get(reverse('index'))
+
+    actual = context_processors.html_lang_attribute(request)
+
+    assert actual['directory_components_html_lang_attribute'] == 'fr'
