84b258b5e2a213d0b4dfe1691ac0815951bb208a
richtier <rikatee@gmail.com>
2018-10-24 12:37:40 +0100

    Add redirect for legal services

--- conf/tests/test_redirects.py
+++ conf/tests/test_redirects.py
@@ -5,6 +5,7 @@ import pytest
     ('/industries/creative/', '/industries/creative-services/'),
     ('/industries/health/', '/industries/healthcare/'),
     ('/industries/tech/', '/industries/technology/'),
+    ('/industries/legal/', '/industries/legal-services/'),
 ))
 def test_redirects(source_url, destination_url, client):
     response = client.get(source_url)
