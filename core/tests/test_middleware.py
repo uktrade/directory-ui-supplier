72ff31bbd44722cef526c7574a99ca6214fc61d8
Kenneth Siu <kenneth.siu@softwire.com>
2019-04-24 12:01:55 +0100

    wip

--- core/tests/test_middleware.py
+++ core/tests/test_middleware.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 
 def test_locale_middleware_installed():
-    expected = 'core.middleware.LocaleQuerystringMiddleware'
+    expected = 'directory_components.middleware.LocaleQuerystringMiddleware'
     assert expected in settings.MIDDLEWARE_CLASSES
