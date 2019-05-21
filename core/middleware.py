643ca476e5607292919afae53f5b847991c3785a
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-05-21 16:40:16 +0100

    isd to reside on root

--- core/middleware.py
+++ core/middleware.py
@@ -17,3 +17,8 @@ class SSLRedirectMiddleware:
 
 class PrefixUrlMiddleware(AbstractPrefixUrlMiddleware):
     prefix = '/trade/'
+
+    def get_redirect_url(self, request):
+        if request.path.startswith('/investment-support-directory/'):
+            return None
+        return super().get_redirect_url(request)
