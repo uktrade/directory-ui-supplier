fe913e8bbc1205da776798c949fad819f6af29d2
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-06-07 12:42:54 +0100

    fixed tests

---
+++ industry/tests/test_industry_tags.py
@@ -0,0 +1,15 @@
+from django.core.urlresolvers import reverse
+
+from industry.templatetags import industry_tags
+
+
+def test_search_url():
+    search_url = industry_tags.search_url(
+        sector_value='AEROSPACE',
+        term='test',
+    )
+
+    assert search_url == (
+                    reverse('find-a-supplier:search') +
+                    '?sectors=AEROSPACE&term=test'
+                )
